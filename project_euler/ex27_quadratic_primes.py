4ab66bdbc7a81d1fe86c9665509156eda7733a31
ralphribeiro <ralph_ribeiro@msn.com>
2021-07-08 19:06:43 -0300

    pe 28

--- project_euler/ex27_quadratic_primes.py
+++ project_euler/ex27_quadratic_primes.py
@@ -1,40 +1,44 @@
-from math import log
+from math import sqrt
 from time import perf_counter_ns
-from itertools import takewhile
 
 
+def fancy_timer(func):
+    def inner(*args):
+        init = perf_counter_ns()
+        ret = func(*args)
+        end = perf_counter_ns()
+        print(func.__name__, ' ', (end - init)/10**6, 'ms')
+        return ret
+    return inner
 
-def is_prime(n):
-    if n == 2 or n == 3:
-        return True
-    if n % 2 == 0 or n < 2:
+
+def eh_primo(n) -> bool:
+    if n < 2 or n % 2 == 0:
         return False
-    for i in range(3, int(n**0.5)+1, 2):   # only odd numbers
-        if n % i == 0:
-            return False
+    elif n == 2:
+        return True
+    else:
+        for x in range(3, int(sqrt(n) + 1), 2):
+            if n % x == 0:
+                return False
     return True
 
-def miller_rabin(n):
-    if not n > 1:
-        return False
-    for all_ in range(2, min(n-2), int(2*(log(n))**2)):
-        x = ...
+
+@fancy_timer
+def calc(a_limite=1000, b_limite=1000):
+    maior = [0, 0, 0]
+    for a in range((a_limite * -1) + 1, a_limite):
+        for b in range(2, b_limite):
+            if eh_primo(b):
+                cont = 0
+                n = 0
+                while eh_primo((n ** 2) + (a * n) + b):
+                    cont += 1
+                    n += 1
+                if cont > maior[0]:
+                    maior = [cont, a, b]
+    return maior[1] * maior[2]
 
 
 if __name__ == "__main__":
-    inicio = perf_counter_ns()
-    euler = [n ** 2 + n + 41 for n in range(0, 40)]
-    print(len(euler))
-    f = list(takewhile(is_prime, euler))
-    assert euler == f
-
-    incredible = [n ** 2 - 79 * n + 1601 for n in range(0, 80)]
-    print(len(incredible))
-    g = list(takewhile(is_prime, incredible))
-    assert incredible == g
-
-    for i in range(1000000):
-        is_prime(i)
-
-    fim = perf_counter_ns() - inicio
-    print('tempo: ', fim / 10**9, ' s')
+    print(calc(1000, 1000))
